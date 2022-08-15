667e996a6fb30b055ae7ac73fb4ea09eacee4331
JD <11360095+JDface@users.noreply.github.com>
2022-08-14 22:39:28 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -39,7 +39,8 @@ streamlit.header(my_data_row)
 
 def insert_row_snowflake(new_fruit):
   with my_cnx.cursor() as my_cur:
-    my_cur.execute("insert into pc_rivery_db.public.fruit_load_list values ('" + new_fruit + "')")
+    #my_cur.execute("insert into pc_rivery_db.public.fruit_load_list values ('" + new_fruit + "')")
+    my_cur.execute("insert into pc_rivery_db.public.fruit_load_list values ('from streamlit')")
     return "Thanks for adding " + new_fruit
   
 # Allow the end user to add a fruit to the list
