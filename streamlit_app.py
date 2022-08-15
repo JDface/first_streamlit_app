69785904aa00816ba32c82d4d97b046897fca122
JD <11360095+JDface@users.noreply.github.com>
2022-08-14 23:04:56 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -46,19 +46,23 @@ except URLError as e:
   streamlit.error()
 #streamlit.write('The user entered', fruit_choice)
 
-streamlit.stop()
+#streamlit.stop()
 
 #import snowflake.connector
 
-my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
-my_cur = my_cnx.cursor()
-my_cur.execute("SELECT * from fruit_load_list")
-my_data_row = my_cur.fetchall()
-#streamlit.text("The fruit load list contains:")
-#streamlit.text(my_data_row)
 streamlit.header("The fruit load list contains:")
-streamlit.header(my_data_row)
+def get_fruit_load_list():
+    with my_cnx.cursor() as my_cur:
+      my_cur.execute("SELECT * from pc_rivery_db.public.fruit_load_list")
+      return my_cur.fetchall()
 
+if streamlit.button('Get Fruit Load List'):
+    my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+    my_data_rows = get_fruit_load_list()
+    #my_cnx.close()
+    streamlit.dataframe(my_data_rows)
+
+    
 def insert_row_snowflake(new_fruit):
   with my_cnx.cursor() as my_cur:
     my_cur.execute("insert into pc_rivery_db.public.fruit_load_list values ('" + new_fruit + "')")
